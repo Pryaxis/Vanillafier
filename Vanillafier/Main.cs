b83c40db242ba4d6a6651e703fb711691169d2e3
Chris <2648373+QuiCM@users.noreply.github.com>
2020-04-13 16:37:21 +0930

    Initial commit

---
+++ Vanillafier/Main.cs
@@ -0,0 +1,132 @@
+﻿using System.IO;
+using System.Linq;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+using TShockAPI.DB;
+
+namespace Vanillafier
+{
+    [ApiVersion(2, 1)]
+    public class Vanillafier : TerrariaPlugin
+    {
+        const string ConfirmationCommand = "vanillafy-confirm";
+        const string GroupName = "Vanillafied";
+        static string TShockConfigPath { get { return Path.Combine(TShock.SavePath, "config.json"); } }
+
+        public override string Author => "Pryaxis";
+        public override string Description => "Instantly configure your server for vanilla gameplay";
+        public override string Name => "Vanillafier";
+
+        public Vanillafier(Main game) : base(game) { }
+
+        public override void Initialize()
+        {
+            Commands.ChatCommands.Add(new Command(Permissions.managegroup, Vanillafy, "vanillafy"));
+        }
+
+        public void Vanillafy(CommandArgs args)
+        {
+            var player = args.Player;
+            if (player == null)
+            {
+                return;
+            }
+
+            if (TShock.Groups.GroupExists(GroupName))
+            {
+                player.SendErrorMessage("The vanillafy command has already been run.");
+                player.SendWarningMessage("If you proceed with running this command, your existing vanilla configuration will be reset.");
+            }
+            else
+            {
+                player.SendWarningMessage("This command will disable most cheat and grief protection for all non-superadmin users.");
+            }
+
+            player.SendWarningMessage($"Are you sure you wish to run this command? Use {TShock.Config.CommandSpecifier}{ConfirmationCommand} to complete configuration.");
+            player.AwaitingResponse.Add(ConfirmationCommand, Confirm);
+        }
+
+        public void Confirm(object obj)
+        {
+            if (obj == null)
+            {
+                return;
+            }
+
+            CommandArgs args = (CommandArgs)obj;
+            var player = args.Player ?? TSPlayer.Server;
+
+            //Get the TShock group manager and setup the new vanilla group
+            GroupManager gm = TShock.Groups;
+            Group group = CreateVanillaGroupObject();
+            if (!gm.GroupExists(GroupName))
+            {
+                gm.AddGroup(name: GroupName, parentname: null, permissions: group.Permissions, chatcolor: Group.defaultChatColor);
+            }
+            else
+            {
+                gm.UpdateGroup(name: GroupName, parentname: null, permissions: group.Permissions, chatcolor: Group.defaultChatColor, suffix: null, prefix: null);
+            }
+            //Retrieve the group again just so that the object state is synced with db state
+            group = gm.GetGroupByName(GroupName);
+
+            //Get the TShock user manager, select all non-superadmin groups, and change their group to the new vanilla group
+            UserManager um = TShock.Users;
+            um.GetUsers().Where(u => u.Group != "superadmin").ForEach(u => um.SetUserGroup(u, GroupName));
+
+            //Update all active player's groups, as long as they're not a superadmin
+            foreach (var ply in TShock.Players)
+            {
+                if (ply?.Group == null || ply.Group is SuperAdminGroup)
+                {
+                    continue;
+                }
+
+                ply.Group = group;
+            }
+
+            //Set the default group for any new guests joining
+            Group.DefaultGroup = group;
+
+            //Update the TShock config file so all new guest users will be assigned to the vanilla group
+            ConfigFile tsConfig = TShock.Config;
+            tsConfig.DefaultGuestGroupName = GroupName;
+            tsConfig.DefaultRegistrationGroupName = GroupName;
+            //Write the config file so that this change persists
+            tsConfig.Write(TShockConfigPath);
+
+            player.SendSuccessMessage("Server has successfully been configured for vanilla gameplay.");
+        }
+        
+        Group CreateVanillaGroupObject()
+        {
+            Group g = new Group(GroupName);
+
+            g.AddPermission("tshock.ignore.*");
+
+            g.AddPermission("tshock.npc.hurttown");
+            g.AddPermission("tshock.npc.startinvasion");
+            g.AddPermission("tshock.npc.startdd2");
+            g.AddPermission("tshock.npc.summonboss");
+
+            g.AddPermission("tshock.tp.others");
+            g.AddPermission("tshock.tp.rod");
+            g.AddPermission("tshock.tp.wormhole");
+
+            g.AddPermission("tshock.world.editspawn");
+            g.AddPermission("tshock.world.modify");
+            g.AddPermission("tshock.world.movenpc");
+            g.AddPermission("tshock.world.paint");
+            g.AddPermission("tshock.world.time.usesundial");
+            g.AddPermission("tshock.world.toggleparty");
+            
+            g.AddPermission("tshock.canchat");
+            g.AddPermission("tshock.partychat");
+            g.AddPermission("tshock.thirdperson");
+            g.AddPermission("tshock.whisper");
+
+            return g;
+        }
+    }
+}
---
+++ Vanillafier/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Vanillafier")]
+[assembly: AssemblyDescription("Terraria Plugin to quickly enable vanilla gameplay in TShock servers")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Pryaxis")]
+[assembly: AssemblyProduct("Vanillafier")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bd785bfc-7357-4784-a2f0-7f4537a3bbdf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
